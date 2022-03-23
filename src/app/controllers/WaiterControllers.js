c578172dc9c724ebc946e98fa22657186f0f81c0
NienLuanNganhCT466 <nienluannganhct466@gmail.com>
2022-03-23 22:54:13 +0700

    built add order waiter

--- src/app/controllers/WaiterControllers.js
+++ src/app/controllers/WaiterControllers.js
@@ -19,7 +19,7 @@ const moment = require('moment')
 class WaiterController {
 
 
-    
+
 
     async homeWaiter(req, res, next) {
         var orderFind = await order.find({})
@@ -35,25 +35,25 @@ class WaiterController {
                     temp = "cam"
                 }
             }
-            if(temp === ""){
+            if (temp === "") {
                 data[i] = {
-                    nameTable : dinnerTableFind[i].name,
-                    color : "White",
-                    slug : dinnerTableFind[i].slug
+                    nameTable: dinnerTableFind[i].name,
+                    color: "White",
+                    slug: dinnerTableFind[i].slug
                 }
             }
-            else if(temp === "xanh"){
+            else if (temp === "xanh") {
                 data[i] = {
-                    nameTable : dinnerTableFind[i].name,
-                    color : "Green",
-                    slug : dinnerTableFind[i].slug
+                    nameTable: dinnerTableFind[i].name,
+                    color: "Green",
+                    slug: dinnerTableFind[i].slug
                 }
             }
             else {
                 data[i] = {
-                    nameTable : dinnerTableFind[i].name,
-                    color : "Orange",
-                    slug : dinnerTableFind[i].slug
+                    nameTable: dinnerTableFind[i].name,
+                    color: "Orange",
+                    slug: dinnerTableFind[i].slug
                 }
             }
         }
@@ -63,12 +63,78 @@ class WaiterController {
 
         var dataFood = await foodMenu.find({ classify: 1 })
         var dataDrink = await foodMenu.find({ classify: 2 })
+        var foodState = []
+        var drinkState = []
+        for (var i = 0; i < dataFood.length; i++) {
+            foodState[i] = {
+                quantity: 1,
+                value: false,
+                slug: dataFood[i].slug
+            }
+        }
+        for (var i = 0; i < dataDrink.length; i++) {
+            drinkState[i] = {
+                quantity: 1,
+                value: false,
+                slug: dataDrink[i].slug
+            }
+        }
         var data = {
             food: dataFood,
-            drink: dataDrink
+            drink: dataDrink,
+            foodState: foodState,
+            drinkState: drinkState
         }
-        res.json(data)        
+        res.json(data)
 
+    }
+
+    async addOrder(req, res, next) {
+        var food = await req.body.food;
+        var drink = await req.body.drink;
+
+        var orderNew = new order();
+        orderNew.dinnerTable = req.body.slugTable;
+        orderNew.note = req.body.note;
+        orderNew.total = req.body.total;
+        orderNew.dinnerTableName = req.body.nameTable;
+        orderNew.orderId = uid();
+        orderNew.state = "Đang xử lý";
+        orderNew.staff = await infoStaff.findOne({ userName: req.body.staff })
+        var index = 0
+        for (var i = 0; i < food.length; i++) {
+            if (food[i].value === true) {
+                var foodFind = await foodMenu.findOne({ slug: food[i].slug })
+                var orderTemp = {}
+                orderTemp.name = foodFind.name
+                orderTemp.price = foodFind.price
+                orderTemp.classify = foodFind.classify
+                orderTemp.description = foodFind.description
+                orderTemp.image = foodFind.image
+                orderTemp.slug = foodFind.slug
+                orderTemp.quantity = food[i].quantity
+                orderNew.order[index] = orderTemp;
+                index++;
+            }
+        }
+        for (var i = 0; i < drink.length; i++) {
+            if (drink[i].value === true) {
+                var drinkFind = await foodMenu.findOne({ slug: drink[i].slug })
+                var orderTemp = {}
+                orderTemp.name = drinkFind.name
+                orderTemp.price = drinkFind.price
+                orderTemp.classify = drinkFind.classify
+                orderTemp.description = drinkFind.description
+                orderTemp.image = drinkFind.image
+                orderTemp.slug = drinkFind.slug
+                orderTemp.quantity = drink[i].quantity
+                orderNew.order[index] = orderTemp;
+                index++;
+            }
+        }
+        var result = await orderNew.save()
+        if(result) res.json("ok")
+        else res.json("error")
     }
 
 
--- src/routes/waiter.js
+++ src/routes/waiter.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const WaiterController = require('../app/controllers/WaiterControllers');
 
-// router.get('/addOrder', WaiterController.addOrder);
-router.get('/getFood', WaiterController.getFood);
 
+
+router.post('/addOrder', WaiterController.addOrder);
+router.get('/getFood', WaiterController.getFood);
 router.get('/homeWaiter', WaiterController.homeWaiter);
 
 module.exports = router;
